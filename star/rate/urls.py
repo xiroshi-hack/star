11a7b90a0decfe9642270adef71ad16961a268c9
abduaziz <104622399+AbduazizBackend@users.noreply.github.com>
2022-09-09 15:16:00 +0500

    Add files via upload

---
+++ star/rate/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from .views import *
+from django.conf import settings
+from django.conf.urls.static import static
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('', home ,name='home'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ star/rate/views.py
@@ -0,0 +1,12 @@
+from multiprocessing import context
+from django.shortcuts import render
+from .models import *
+
+def home(request):
+    rate = RateStars.objects.filter(score=2).order_by("?").first()
+    
+    context = {
+        "rate": rate,
+    }
+    
+    return render(request, 'home.html', context)
